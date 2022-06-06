11cfe33ca3ea2cfa1b3203ed5d863b252cf670a6
DudkoMatt <github.dudko.matt@mail.ru>
2022-06-06 12:33:47 +0300

    chore: Add base code structure

---
+++ src/main/java/ru/dudkomv/neoflexpractice/NeoflexPracticeSummer2022Application.java
@@ -0,0 +1,13 @@
+package ru.dudkomv.neoflexpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoflexPracticeSummer2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoflexPracticeSummer2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dudkomv/neoflexpractice/NeoflexPracticeSummer2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dudkomv.neoflexpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoflexPracticeSummer2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
